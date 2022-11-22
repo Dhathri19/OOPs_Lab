95c57e5b32d2eeb8fb7296a3894675bffd7831f1
Dhathri19 <medadhathri19@gmail.com>
2022-11-22 16:44:12 +0530

    First lab

---
+++ Lab_1/Array.java
@@ -0,0 +1,8 @@
+class array{
+    public static void main(String args[]){
+        int a[] = {2, 5, 6, 7, 10};
+        for(int i = 0; i <= a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
---
+++ Lab_1/Summ.java
@@ -0,0 +1,7 @@
+class summation{
+    public static void main(String args[]){
+        int a = 10, b = 30;
+        int sum = a + b;
+        System.out.println("Sum of "+a+ " and "+b+" is "+sum);
+    }
+}
